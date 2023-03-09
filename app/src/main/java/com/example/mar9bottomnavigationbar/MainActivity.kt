dc08a8ca6e016ea24b564d092e1c9c4d003ebfc4
hieuchathichai <minghieuu@gmail.com>
2023-03-09 20:19:58 +0700

    add all file from stage to commit

---
+++ app/src/main/java/com/example/mar9bottomnavigationbar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mar9bottomnavigationbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

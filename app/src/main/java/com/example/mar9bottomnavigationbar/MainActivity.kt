cc64b1724ea015cdd01a839313e76ccb813f6973
hieuchathichai <minghieuu@gmail.com>
2023-03-09 20:36:46 +0700

    fixed text error

--- app/src/main/java/com/example/mar9bottomnavigationbar/MainActivity.kt
+++ app/src/main/java/com/example/mar9bottomnavigationbar/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val name="Vu MInh Hieu"
+        val fix="had fix error"
     }
 }
